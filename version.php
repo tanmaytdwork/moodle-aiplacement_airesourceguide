16f014a3caa574ca8c3b87b668f41057fa0775fe
TD <tanmaydeshmukhtd1@gmail.com>
2026-04-03 01:41:11 +0530

    version update

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiplacement_airesourceguide';
-$plugin->version   = 2026031005;
+$plugin->version   = 2026031006;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.2';
